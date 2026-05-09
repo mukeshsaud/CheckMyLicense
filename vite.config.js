d99eca70e91b02094273bf82fdaea2fe5e1f048e
mukesh saud <mukeshsaud@mukeshs-MacBook-Air.local>
2026-05-09 23:52:20 +0545

    frontend set1

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({mode})=>{
+  const env=loadEnv(mode,process.cwd(),'')
+  return{
   plugins: [react(),tailwindcss(),],
   server:{open:true,
     proxy:{
       "/db":{
-        target:`${import.meta.env.RENDERBACKEND_URL}`,
+        target:env.VITE_RENDERBACKEND_URL,
         changeOrigin:true
       }
     }
   },
+}
 })
